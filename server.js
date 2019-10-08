6c7e7efb8d61dd1e0acaed565e34ffa14ce78315
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-10-08 18:34:12 -0500

    Connect app with express and make the initial route work and configure accordingly.

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
-const knex = require('./knex/knex.js');
+const knex = require('./knexfile.js');
 const app = express();
 
-app.get('/tasks', (req, res) => {
-  res.send('You got here')
+app.get('/', (req, res) => {
+    res.send(musicQuery(res))
 });
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
+
+
+function musicQuery () {
+  var music = knex.raw('SELECT artist, title, track_title AS track FROM tracks JOIN artist ON tracks.artist_id = artist.id JOIN album ON tracks.album_id = album.id')
+  return music
+}
